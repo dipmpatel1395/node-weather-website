fa92feed3559a0f2b7e1db92446f3bf445199225
Dipkumar Patel <64330951+dipmpatel1395@users.noreply.github.com>
2020-05-03 20:47:01 -0600

    Removed Unncessary consol.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-//console.log('client side JS loded')
-
 const weatherForm = document.querySelector('form')
 const search =document.querySelector('input')
 const messageOne = document.querySelector('#p1')
